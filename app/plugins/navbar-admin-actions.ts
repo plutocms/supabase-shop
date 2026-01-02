f7d69aeb03523a2284a340b3edc38f3a48702cfd
Victor Ribeiro <devjvribeiro@gmail.com>
2026-01-02 01:08:59 -0300

    feat: add sidebar actions for admin navigation

--- app/plugins/navbar-admin-actions.ts
+++ app/plugins/navbar-admin-actions.ts
@@ -1,7 +1,32 @@
 import NavbarAdminActions from '../components/NavbarAdminActions.vue'
 
-export default defineNuxtPlugin(() => {
-  const { addAction } = useNavbarAdminActions()
+const sidebarActions = [
+  {
+    label: 'Products',
+    href: '/admin/products',
+    icon: 'lucide:box',
+    onSelect: () => {
+      navigateTo('/admin/products')
+    },
+    defaultOpen: true,
+    children: [
+      {
+        label: 'Create new product',
+        href: '/admin/product/new',
+      },
 
-  addAction(markRaw(NavbarAdminActions))
+      {
+        label: 'All products',
+        href: '/admin/products',
+      },
+    ],
+  },
+]
+
+export default defineNuxtPlugin(() => {
+  const { addAction: addNavbarAdminActions } = useNavbarAdminActions()
+  const { addAction: addSidebarAdminActions } = useSidebarAdminActions()
+
+  addNavbarAdminActions(markRaw(NavbarAdminActions))
+  addSidebarAdminActions(sidebarActions)
 })
