7c46e1e3a144377e0c0cfe4eea27dff8d592511a
Victor Ribeiro <devjvribeiro@gmail.com>
2026-02-09 02:09:22 -0300

    fix: removed environment based extends

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  $development: {
-    extends: [
-      ['../../plutocms/utils/'],
-      ['../../plutocms/supabase/', { install: true }],
-    ],
-  },
+  extends: [
+    ['github:plutocms/utils'],
+    ['github:plutocms/supabase', { install: true }],
+  ],
 
   $meta: {
     name: 'supabase-shop',
-  },
-
-  $production: {
-    extends: [
-      ['github:plutocms/utils'],
-      ['github:plutocms/supabase', { install: true }],
-    ],
   },
 
   css: ['#layers/supabase-shop/app/assets/css/tailwind.css'],
