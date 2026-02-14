d20846b5cc646ca846174c485c4243f45560289a
Victor Ribeiro <devjvribeiro@gmail.com>
2026-02-14 20:13:09 -0300

    fix: removed layer dep auto-install

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   extends: [
     ['github:plutocms/utils'],
-    ['github:plutocms/supabase', { install: true }],
+    ['github:plutocms/supabase'],
   ],
 
   $meta: {
